914a586dc62838ac3ec7e6c84a8068a9d84e87b0
zhenyezhao <86699069+zhenyezhao@users.noreply.github.com>
2023-04-23 19:00:47 -0700

    Add files via upload

---
+++ throw.js
@@ -0,0 +1,11 @@
+AFRAME .registerComponent("throw",{
+    init:function(){
+        this.bowling-balls
+        var throw=document.querySelector(#bowling-balls)
+
+    },
+bowlingBalls:function(){
+
+}
+})
+ 
